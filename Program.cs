daf2d3b22b7aa1dbe1c75024050932f3682fa34c
Ivan <saff@inbox.ru>
2022-08-21 13:49:13 +0300

    Secont task

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*Console.WriteLine("Hello, World!");
+Console.Write("Введите число : ");
+int number = int.Parse(Console.ReadLine());
+int sqr = number * number;
+Console.WriteLine("Результат = {0}", sqr);
+Console.WriteLine($"Результат = {sqr}");
+Console.WriteLine($"Результат = {Math.Pow(number, 2)}");
+// возвращает вщиду корректнее перевести в целые
+int result = Convert.ToInt32(Math.Pow(number, 2));
+Console.WriteLine($"Результат = {result}");
+*/
+
+
+/*
+1. Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+a = 25; b = 5 -> да 
+a = 2; b = 10 -> нет 
+a = 9; b = -3 -> да 
+a = -3; b = 9 -> нет
+*/
