0933619ec609908e869121d6dc4ddc2667f942a2
Ivan <saff@inbox.ru>
2022-08-28 20:17:34 +0300

    homework for 3 seminar

---
+++ seminar3/task19/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число 
+// и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите пятизначное число : ");
+int num = int.Parse(Console.ReadLine());
+
+if (num < -99999  || num > 99999) Console.Write("ОШИБКА! Число не пятизначное");
+else
+{
+    // calculate digit for 0, 1, 3, 4 position of number
+    int b0 = num % 10;
+    int b1 = (num % 100) / 10;
+    int b3 = (num % 10000) / 1000;
+    int b4 = (num % 100000) / 10000;
+
+ //   Console.WriteLine($"b0 = {b0}, b1 = {b1}, b3 = {b3}, b4 = {b4}");
+
+    if (b0 == b4 && b1 == b3) Console.WriteLine("Да");
+    else Console.WriteLine("Нет");
+    
+};
+
---
+++ seminar3/task21/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// Enter x,y,z point A
+Console.WriteLine("Ввод координат первой точки");
+
+Console.Write("Введите x1 : ");
+int x1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите y1 : ");
+int y1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите z1 : ");
+int z1 = int.Parse(Console.ReadLine());
+
+// Enter x,y,z point B
+Console.WriteLine("Ввод координат второй точки");
+
+Console.Write("Введите x2 : ");
+int x2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите y2 : ");
+int y2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите z2 : ");
+int z2 = int.Parse(Console.ReadLine());
+
+double d = Math.Sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2) + (z1-z2)*(z1-z2));
+
+Console.WriteLine($"Расстояние 3D: {d:f2} ");
+
+//double dist = Math.Round(d, 3);
+//Console.WriteLine($"Расстояние : {dist} ");
