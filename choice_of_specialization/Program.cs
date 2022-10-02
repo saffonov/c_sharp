3dfb784d9807a83514ab46067061fd714d51351e
Ivan <saff@inbox.ru>
2022-10-02 14:14:27 +0300

    add generate string

--- choice_of_specialization/Program.cs
+++ choice_of_specialization/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//
+string[] GetArray(int m, int n) // m - number of string; n - number char in string
+{
+    char[] letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+    string[] result = new string[m];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+           int num_letters = new Random().Next(0, letters.Length - 1);
+           result[i] += letters[num_letters]; 
+        }    
+    }
+    return result;
+}
+
+
+Console.WriteLine("START");
+
+Console.Write("Введите количество строк:");
+int m = int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество символов:");
+int n = int.Parse(Console.ReadLine());
+
+string[] arr = GetArray(m, n);
+Console.WriteLine("Исходный массив");
+Console.Write(String.Join(", ", arr));
+//Console.Write(String.Join(", ", GetRNDarray(n, A, B)));
+
+
+
